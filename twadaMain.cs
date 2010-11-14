308fae52572c1a5399e5397a618bc7cea5fee745
Elijah Ellis <elijah.w.ellis@gmail.com>
2010-11-13 20:16:10 -0500

    Added twadaMain.cs source

---
+++ twadaMain.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace twada
+{
+    class twadaMain
+    {
+        static void Main(string[] args)
+        {
+            var UserOutput = Console.Out;
+            Console.ForegroundColor = ConsoleColor.Blue;Console.WriteLine("twada, a vague Twitter console application...");
+            Console.Clear();
+            if (args.Length > 0)
+            {
+                if (args[0] == "-pub")
+                {
+                    UserOutput.WriteLine("Retrieving public timeline...");
+                    var ThreadStart = new System.Threading.ThreadStart(GetTwitterPublic);
+                    var BeginGet = new System.Threading.Thread(ThreadStart);
+                    BeginGet.Start();
+                }
+            }
+            else
+            {
+                UserOutput.WriteLine("twada started with no arguments...");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+            }
+            
+        }
+
+        //ThreadStart void-function to get the "public_timeline" response of the Twitter API
+        static public void GetTwitterPublic()
+        {
+
+            var twRequest = System.Net.HttpWebRequest.Create("http://api.twitter.com/1/statuses/public_timeline.xml");
+            twRequest.ImpersonationLevel = System.Security.Principal.TokenImpersonationLevel.Anonymous;
+            twRequest.AuthenticationLevel = System.Net.Security.AuthenticationLevel.None;
+
+            var twDocument = new XmlDocument();
+            twDocument.LoadXml(new System.IO.StreamReader(twRequest.GetResponse().GetResponseStream()).ReadToEnd());
+            int i;
+            var Statuses = GetStatuses(twDocument);
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            for (i = 0; i != Statuses.Length ; i += 1)
+            {
+                Console.Out.WriteLine(Statuses[i]);
+            }
+
+            twRequest = null;
+            twDocument = null;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+        }
+
+        //Function to parse statuses into an array of string variables
+        static public string[] GetStatuses(XmlDocument XmlResponse)
+        {
+            var ListString = new List<string>();
+            var twDocument = XmlResponse;
+            int i;
+            for (i = 0; i != twDocument.SelectNodes("//statuses/status").Count; i += 1)
+            {
+                ListString.Add(twDocument.SelectNodes("//statuses/status")[i]["text"].InnerText);
+            }
+            return ListString.ToArray();
+        }
+    }
+}
